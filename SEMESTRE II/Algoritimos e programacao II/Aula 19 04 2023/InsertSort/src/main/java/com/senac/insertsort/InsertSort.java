1c94bc4df7e93e67c58c160f2dabd648cdf2e70f
ArthurBerdusco <107810609+ArthurBerdusco@users.noreply.github.com>
2023-04-19 23:30:25 -0300

    Aula de alg ii

---
+++ SEMESTRE II/Algoritimos e programacao II/Aula 19 04 2023/InsertSort/src/main/java/com/senac/insertsort/InsertSort.java
@@ -0,0 +1,48 @@
+package com.senac.insertsort;
+
+import java.util.Random;
+
+public class InsertSort {
+
+    public static String[] gerarPalavras() {
+        String[] palavras = new String[30];
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random random = new Random();
+
+        for (int i = 0; i < palavras.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 5; j++) {
+                int index = random.nextInt(letras.length());
+                String palavra = sb.append(letras.charAt(index)).toString();
+                palavras[i] = palavra;
+            }
+        }
+
+        return palavras;
+    }
+
+    public static String[] organizarPalavras(String[] palavras) {
+        for (int i = 1; i < palavras.length; i++) {
+            String valorAtual = palavras[i];
+            int j = i - 1;
+            while (j >= 0 && palavras[j].compareTo(valorAtual) > 0) {
+                palavras[j + 1] = palavras[j];
+                j--;
+            }
+            palavras[j + 1] = valorAtual;
+        }
+        return palavras;
+    }
+
+    public static void lerPalavras(String[] palavras) {
+        for (int i = 0; i < palavras.length; i++) {
+            System.out.println(palavras[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] palavras = gerarPalavras();
+        
+        lerPalavras(organizarPalavras(palavras));
+    }
+}
