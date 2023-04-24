c8529987f8076b0f3d1c65f118ba5dd3d963ef61
ArthurBerdusco <107810609+ArthurBerdusco@users.noreply.github.com>
2023-04-24 19:55:13 -0300

    Arthur

---
+++ SEMESTRE II/Projeto Integrador/Aulas/aula 24 04 2023/exemploDatas/src/main/java/com/senac/exemplodatas/views/model/NotaFiscal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.exemplodatas.views.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jsilva91
+ */
+public class NotaFiscal {
+    
+    private int idNota;
+    private int numeroNota;
+    private int valorNota;
+    private Date data;
+}
